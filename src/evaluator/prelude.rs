015c916880e3955e1d0aa8fb671924dda671d7b6
Alex Stokes <r.alex.stokes@gmail.com>
2019-01-13 16:55:12 -0600

    Add `=`, `inc` and `dec`

--- src/evaluator/prelude.rs
+++ src/evaluator/prelude.rs
@@ -102,6 +102,46 @@ fn prn(args: Vec<Expr>) -> Result<Expr> {
     Ok(Expr::Nil)
 }
 
+fn eq(args: Vec<Expr>) -> Result<Expr> {
+    args.split_first()
+        // TODO fix possible many arity
+        .ok_or(Error::WrongArity(1, args.len()))
+        .and_then(|(first, rest)| {
+            for form in rest {
+                if first != form {
+                    return Ok(Expr::Bool(false));
+                }
+            }
+            Ok(Expr::Bool(true))
+        })
+}
+
+fn inc(args: Vec<Expr>) -> Result<Expr> {
+    if args.len() != 1 {
+        return Err(Error::IncorrectArguments);
+    }
+
+    args.get(0)
+        .ok_or(Error::IncorrectArguments)
+        .and_then(|number| match number {
+            Expr::Number(number) => Ok(Expr::Number(number + 1)),
+            _ => Err(Error::IncorrectArguments),
+        })
+}
+
+fn dec(args: Vec<Expr>) -> Result<Expr> {
+    if args.len() != 1 {
+        return Err(Error::IncorrectArguments);
+    }
+
+    args.get(0)
+        .ok_or(Error::IncorrectArguments)
+        .and_then(|number| match number {
+            Expr::Number(number) => Ok(Expr::Number(number - 1)),
+            _ => Err(Error::IncorrectArguments),
+        })
+}
+
 static PRELUDE_BINDINGS: &[(&str, &str, HostFn)] = &[
     ("+", "add", add),
     ("-", "sub", sub),
@@ -109,6 +149,9 @@ static PRELUDE_BINDINGS: &[(&str, &str, HostFn)] = &[
     ("/", "div", div),
     ("list", "list", list),
     ("prn", "prn", prn),
+    ("=", "equal", eq),
+    ("inc", "increment", inc),
+    ("dec", "decrement", dec),
 ];
 
 pub fn env() -> Env<'static> {
